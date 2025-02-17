3dd86c952d8bb492bdfee3afcdc76a76b087ec8e
Teetwothefirst <dexterdavid835@gmail.com>
2025-02-17 07:38:12 -0800

    sidepanel colors

--- client/eal/src/Pages/Admin.jsx
+++ client/eal/src/Pages/Admin.jsx
@@ -12,12 +12,12 @@ const Admin = ()=>{
             <div className="row">
                 <div className="col-lg-2 shadow-sm side-panel-color">
                     <div className="sidepanel h-screen px-3 pt-3">
-                        <div className="">
+                        <div className="flex flex-col gap-3">
                             <div className="dashboard">
-                                <Link to={`/D/main`} className=""><i className="bi bi-grid-1x2-fill"></i>Dashboard</Link>
+                                <Link to={`/D/main`} className="text-white flex gap-3 items-center"><i className="bi bi-grid-1x2-fill"></i> <h6 className='m-0'> Dashboard</h6></Link>
                             </div>
                             <div className="chat">
-                                <Link to={`/D/chat`} className=""><i className="bi bi-telegram"></i>Chat</Link>
+                                <Link to={`/D/chat`} className="text-white flex gap-3 items-center"><i className="bi bi-telegram"></i> <h6 className="m-0">Chat</h6></Link>
                             </div>
                         </div>
                     </div>
